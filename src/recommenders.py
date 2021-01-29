0a9612959cfea28393c5ab0e2588adb20faa0eeb
Vitaly Gladyshev <48773035+VitalyGladyshev@users.noreply.github.com>
2021-01-29 15:10:21 +0300

    Класс для выдачи рекомендаций

--- src/recommenders.py
+++ src/recommenders.py
@@ -30,10 +30,10 @@ class MainRecommender:
         Матрица взаимодействий user-item
     """
 
-    def __init__(self, data, weighting=True):
+    def __init__(self, data, item_features, weighting=True):
         # your_code. Это не обязательная часть. Но если вам удобно что-либо посчитать тут - можно это сделать
 
-        self.prep_data = prefilter_items(data)
+        self.prep_data = prefilter_items(data, item_features)
         self.user_item_matrix = self.prepare_matrix(self.prep_data)  # pd.DataFrame
         self.id_to_itemid, self.id_to_userid, \
         self.itemid_to_id, self.userid_to_id = self.prepare_dicts(self.user_item_matrix)
@@ -99,16 +99,28 @@ class MainRecommender:
         return model
 
     
-    def get_recommendations(self, user, rec_num=5):
+    def get_als_recommendations(self, user, rec_num=5):
         """Рекомендуем топ-N товаров"""
 
         res = [self.id_to_itemid[rec[0]] for rec in 
                         self.model.recommend(userid=self.userid_to_id[user], 
-                                            user_items=csr_matrix(self.user_item_matrix).tocsr(),   # на вход user-item matrix
-                                            N=rec_num, 
-                                            filter_already_liked_items=False, 
-                                            filter_items=[self.itemid_to_id[999999]],  # !!! 
-                                            recalculate_user=True)]
+                                             user_items=csr_matrix(self.user_item_matrix).tocsr(),   # на вход user-item matrix
+                                             N=rec_num, 
+                                             filter_already_liked_items=False, 
+                                             filter_items=[self.itemid_to_id[999999]],  # !!! 
+                                             recalculate_user=True)]
+        return res
+    
+    def get_own_recommendations(self, user, rec_num=5):
+        """Рекомендуем топ-N собственных товаров"""
+
+        res = [self.id_to_itemid[rec[0]] for rec in 
+                    self.own_recommender.recommend(userid=self.userid_to_id[user], 
+                                                   user_items=csr_matrix(self.user_item_matrix).tocsr(),   # на вход user-item matrix
+                                                   N=rec_num, 
+                                                   filter_already_liked_items=False, 
+                                                   filter_items=[self.itemid_to_id[999999]],  # !!! 
+                                                   recalculate_user=True)]
         return res
     
     def get_similar_items_recommendation(self, user, rec_num=5):
@@ -124,7 +136,7 @@ class MainRecommender:
             res.add(self.id_to_itemid[self.model.similar_items(self.itemid_to_id[item], N=1)[0][0]])
 
 #         assert len(res) == N, 'Количество рекомендаций != {}'.format(N)
-        return res
+        return list(res)
 
     def get_similar_users_recommendation(self, user, rec_num=5):
         """Рекомендуем топ-N товаров, среди купленных похожими юзерами"""
@@ -133,12 +145,12 @@ class MainRecommender:
                                                                                         N=rec_num+1)[1:rec_num+1]]
         res = set()
         for user_in in sim_users_list:
-            res.add(list(self.get_recommendations(user_in, rec_num=1))[0])
+            res.add(list(self.get_als_recommendations(user_in, rec_num=1))[0])
             
         i = 2
         while len(res) < rec_num:
-            res.add(list(self.get_recommendations(sim_users_list[1], rec_num=i))[i-1])
+            res.add(list(self.get_als_recommendations(sim_users_list[1], rec_num=i))[i-1])
             i += 1
         
 #         assert len(res) == N, 'Количество рекомендаций != {}'.format(N)
-        return res
+        return list(res)
